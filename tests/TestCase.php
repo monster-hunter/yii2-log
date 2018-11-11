69ddd9feaeacdcdc890b5d1f8e0697c68b181de1
zacksleo <zacksleo@gmail.com>
2018-11-11 12:42:38 +0800

    test(config): fix config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -67,15 +67,17 @@ class TestCase extends PHPUnit_Framework_TestCase
                 'log' => [
                     'traceLevel' => YII_DEBUG ? 3 : 0,
                     'targets' => [
-                        'class' => 'yii\log\DbTarget',
-                        'levels' => ['error', 'warning'],
-                        'except' => ['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
-                        'prefix' => function () {
-                            $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
-                            return sprintf('[%s][%s]', Yii::$app->id, $url);
-                        },
-                        'logVars' => [],
-                        'logTable' => '{{%system_log}}'
+                        'db' => [
+                            'class' => 'yii\log\DbTarget',
+                            'levels' => ['error', 'warning'],
+                            'except' => ['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+                            'prefix' => function () {
+                                $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+                                return sprintf('[%s][%s]', Yii::$app->id, $url);
+                            },
+                            'logVars' => [],
+                            'logTable' => '{{%system_log}}'
+                        ]
                     ],
                 ],
             ],
@@ -112,15 +114,17 @@ class TestCase extends PHPUnit_Framework_TestCase
                 'log' => [
                     'traceLevel' => YII_DEBUG ? 3 : 0,
                     'targets' => [
-                        'class' => 'yii\log\DbTarget',
-                        'levels' => ['error', 'warning'],
-                        'except' => ['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
-                        'prefix' => function () {
-                            $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
-                            return sprintf('[%s][%s]', Yii::$app->id, $url);
-                        },
-                        'logVars' => [],
-                        'logTable' => '{{%system_log}}'
+                        'db' => [
+                            'class' => 'yii\log\DbTarget',
+                            'levels' => ['error', 'warning'],
+                            'except' => ['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+                            'prefix' => function () {
+                                $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+                                return sprintf('[%s][%s]', Yii::$app->id, $url);
+                            },
+                            'logVars' => [],
+                            'logTable' => '{{%system_log}}'
+                        ]
                     ],
                 ],
             ],
